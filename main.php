1ef546da7fc60cb0ff3c51eb80f0432c5dedd9b4
Mateusz Wnuk <mateusz2003w@gmail.com>
2025-01-31 16:04:01 +0100

    added .gitignore

--- main.php
+++ main.php
@@ -1,1 +1,19 @@
 <?php
+
+$number = rand(1, 100);
+$maxAttempts = 0;
+
+$level = readline("Wybierz poziom trudności: ");
+
+switch ($level){
+    case 'easy':
+        $maxAttempts = 10;
+        break;
+    case'medium':
+        $maxAttempts = 5;
+        break;
+    case 'hard':
+        $maxAttempts = 3;
+        break;
+}
+
