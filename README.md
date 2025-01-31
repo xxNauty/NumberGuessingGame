035b9bc9dab14417676388da2267d5218040c4d9
Mateusz Wnuk <mateusz2003w@gmail.com>
2025-01-31 17:04:50 +0100

    working game, added README.md

---
+++ README.md
@@ -0,0 +1,2 @@
+# NumberGuessingGame
+This is the project of the game where you have to guess a number between 1 and 100, inspired by [roadmap.sh](https://roadmap.sh/projects/number-guessing-game)
--- main.php
+++ main.php
@@ -3,17 +3,45 @@
 $number = rand(1, 100);
 $maxAttempts = 0;
 
-$level = readline("Wybierz poziom trudności: ");
+$correctLevel = false;
+do {
+    echo("1. Niski(10 prób)\n2. Średni(5 prób)\n3. Wysoki(3 próby)\n");
+    $level = readline("Wybierz poziom trudności: ");
+    echo("\n");
 
-switch ($level){
-    case 'easy':
-        $maxAttempts = 10;
+    switch ($level){
+        case 'Niski':
+            $maxAttempts = 10;
+            $correctLevel = true;
+            break;
+        case 'Średni':
+            $maxAttempts = 5;
+            $correctLevel = true;
+            break;
+        case 'Wysoki':
+            $maxAttempts = 3;
+            $correctLevel = true;
+            break;
+        default:
+            echo("Błędna wartość, spróbuj ponownie.\n");
+            break;
+    }
+} while (!$correctLevel);
+
+for($i = 0; $i < $maxAttempts; $i++){
+    $attempt = readline("Zgadnij liczbę:");
+
+    if($attempt == $number){
+        echo(sprintf("Brawo, zgadłeś!\nUdało ci się to za %d razem\n", $i + 1));
         break;
-    case'medium':
-        $maxAttempts = 5;
-        break;
-    case 'hard':
-        $maxAttempts = 3;
-        break;
+    } else {
+        if($attempt > $number){
+            echo("Za dużo!\n");
+        } else {
+            echo("Za mało!\n");
+        }
+    }
 }
+
+echo("Koniec gry, wykorzystałeś wszystkie próby. Szukana liczba to $number\n");
 
